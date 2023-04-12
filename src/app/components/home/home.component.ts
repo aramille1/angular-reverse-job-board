d7f01f2a5884548dd9c7ce8500f67247aa035002
aramille1 <aramille@googlemail.com>
2023-04-12 18:30:07 +0200

    fix: home page added all engineers

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
+import { EngineerService } from 'src/app/services/engineer-service/engineer.service';
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,36 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  engineers = [
-    {
-      name: "Joana",
-      id:1,
-      hero: 'Great to be here',
-      avatar: 'https://static.vecteezy.com/system/resources/thumbnails/001/993/889/small/beautiful-latin-woman-avatar-character-icon-free-vector.jpg',
-      bio: "I have been a web/software developer for the past 5 years both as a freelancer, hobbyist, intern and a full-time employee. My natural strengths are curiosity and eagerness to learn and this has led me to look at interesting websites and try to replicate their functionality in areas such as design",
-      searchStatus: {
-        activelylooking: true
-      }
-    },
-    {
-      name: "Lee",
-      id:2,
-      hero: 'Senior Angular dev',
-      avatar: 'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375__340.png',
-      bio: "As a full-stack developer with a strong focus on Ruby on Rails, I have a deep understanding of how to build and maintain high-quality, scalable web applications. I am constantly striving to improve my skills and stay up-to-date with the latest technologies, which allows me to bring new and innovative solutions to the table.",
-      searchStatus: {
-        activelylooking: true
-      }
-    },
-    {
-      name: "Ahmad",
-      id:3,
-      hero: 'Junior Angular dev',
-      avatar: 'https://img.freepik.com/free-icon/athlete_318-186007.jpg',
-      bio: "As a full-stack developer with a strong focus on Ruby on Rails, I have a deep understanding of how to build and maintain high-quality, scalable web applications. I am constantly striving to improve my skills and stay up-to-date with the latest technologies, which allows me to bring new and innovative solutions to the table.",
-      searchStatus: {
-        activelylooking: true
-      }
-    },
-  ]
+  engineers = new Array<any>();
+  private engineersSub: Subscription
+
+  constructor(private engineerService: EngineerService){}
+
+  ngOnInit(): void {
+    this.engineersSub = this.engineerService.getAllEngineers().subscribe({
+      next: (res) =>{
+        console.log(res.engineers)
+        this.engineers = res.engineers
+      },
+      error: (err) => console.error(err)
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.engineersSub.unsubscribe();
+  }
 }
