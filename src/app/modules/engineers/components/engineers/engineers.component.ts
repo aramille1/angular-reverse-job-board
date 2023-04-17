d155b0ea2fb23f67cf7c957804b57e2c528da5a6
aramille1 <aramille@googlemail.com>
2023-04-17 14:34:46 +0200

    fix: blur on last three engineers if not member

--- src/app/modules/engineers/components/engineers/engineers.component.ts
+++ src/app/modules/engineers/components/engineers/engineers.component.ts
@@ -8,68 +8,70 @@ import { EngineerService } from 'src/app/services/engineer-service/engineer.serv
 @Component({
   selector: 'app-engineers',
   templateUrl: './engineers.component.html',
-  styleUrls: ['./engineers.component.scss']
+  styleUrls: ['./engineers.component.scss'],
 })
 export class EngineersComponent {
   engineers = new Array<any>();
   limit: number = 10;
   page: number = 1;
   total: number = 13;
-  pagesCount: Number[] = [1,2,3];
+  pagesCount: Number[] = [1, 2, 3];
   startIndex = 0;
   endIndex = 5;
-  status:boolean = false;
-  isMember: boolean = true;
-  countries:any = [];
+  status: boolean = false;
+  isMember: boolean = false;
+  countries: any = [];
   selectedCountry: string;
   keyword = 'name';
   data$: Observable<any>;
-  data: any = []
-  private getMyProfileSub: Subscription
-  private getEngineersSub: Subscription
+  data: any = [];
+  private getMyProfileSub: Subscription;
+  private getEngineersSub: Subscription;
   constructor(
     private engineerService: EngineerService,
     private auth: AuthService,
     private http: HttpClient,
     private loadingBar: LoadingBarService
-    ){}
+  ) {}
 
   public config: PaginationInstance = {
-      id: 'custom',
-      itemsPerPage: 10,
-      currentPage: 1
+    id: 'custom',
+    itemsPerPage: 10,
+    currentPage: 1,
   };
   ngOnInit(): void {
     this.loadingBar.start();
-    this.http.get("https://restcountries.com/v3.1/all?fields=name,flags").subscribe({
-      next: data => {
-        for (const [key, value] of Object.entries(data)){
-          this.data.push({id: Number(key+1), name: value.name.common, flag: value.flags.svg})
-        }
-        console.log(this.data)
-      },
-      error: err => console.error(err)
-    })
+    this.http
+      .get('https://restcountries.com/v3.1/all?fields=name,flags')
+      .subscribe({
+        next: (data) => {
+          for (const [key, value] of Object.entries(data)) {
+            this.data.push({
+              id: Number(key + 1),
+              name: value.name.common,
+              flag: value.flags.svg,
+            });
+          }
+          console.log(this.data);
+        },
+        error: (err) => console.error(err),
+      });
     this.getMyProfileSub = this.auth.getMyProfile().subscribe({
-      next:res => {
-        if(res.type === "recruiter" && res.user.IsMember){
-          this.isMember = false
+      next: (res) => {
+        if (res.type === 'recruiter' && res.user.IsMember) {
+          this.isMember = true;
         }
-      }
-    })
+      },
+    });
     // TODO need to get number of all engineers and set it in total
     this.getEngineers();
   }
 
-  getPageAmout(length: number){
+  // getPageAmout(length: number){
 
-    console.log(this.engineers.length)
-    return new Array(length/20)
-  }
-
-  startLoading() {
-
-  }
+  //   console.log(this.engineers.length)
+  //   return new Array(length/20)
+  // }
 
   // getIndex(pageIndex: number){
   // //   this.startIndex = pageIndex * 5;
@@ -89,72 +91,63 @@ export class EngineersComponent {
   //   this.getEngineers()
   // }
 
-
-
-  getEngineers(){
-    this.getEngineersSub = this.engineerService.getEngineers(this.page, this.limit, this.selectedCountry = '').subscribe(res => {
-      console.log(res.engineers)
-      this.engineers = res.engineers;
-      this.loadingBar.stop();
-
-  });
+  getEngineers() {
+    this.getEngineersSub = this.engineerService
+      .getEngineers(this.page, this.limit, (this.selectedCountry = ''))
+      .subscribe((res) => {
+        console.log(res.engineers);
+        this.engineers = res.engineers;
+        this.loadingBar.stop();
+      });
   }
 
-
-
-
-  pageChangeEvent(event: number){
+  pageChangeEvent(event: number) {
     this.page = event;
-    this.getEngineers()
+    this.getEngineers();
   }
 
   ngOnDestroy(): void {
-    this.getMyProfileSub.unsubscribe()
-    this.getEngineersSub.unsubscribe()
+    this.getMyProfileSub.unsubscribe();
+    this.getEngineersSub.unsubscribe();
   }
 
   // onCheck(country:string){
   //   this.selectedCountry = country
   //   console.log(this.selectedCountry)
-
-
   // }
 
-  applyFilter(){
-    if(this.selectedCountry){
-      this.getEngineersSub = this.engineerService.getEngineers(this.page, this.limit, this.selectedCountry).subscribe(res => {
-        this.engineers = res.engineers;
+  applyFilter() {
+    if (this.selectedCountry) {
+      this.getEngineersSub = this.engineerService
+        .getEngineers(this.page, this.limit, this.selectedCountry)
+        .subscribe((res) => {
+          this.engineers = res.engineers;
 
-        console.log(res.engineers);
-
-    });
+          console.log(res.engineers);
+        });
     }
   }
 
-
-
-
-    selectEvent(item: any) {
-      // do something with selected item
-      console.log(item)
-      this.getEngineersSub = this.engineerService.getEngineers(this.page, this.limit, item.name).subscribe(res => {
-
+  selectEvent(item: any) {
+    // do something with selected item
+    console.log(item);
+    this.getEngineersSub = this.engineerService
+      .getEngineers(this.page, this.limit, item.name)
+      .subscribe((res) => {
         // console.log(res.engineers.length);
         // this.total = res.engineers.length
-        console.log(res.engineers)
-        this.engineers = res.engineers
-
-
-    });
-    }
-
-    // onChangeSearch(val: string) {
-    //   console.log(val)
-    //   // fetch remote data from here
-    //   // And reassign the 'data' which is binded to 'data' property.
-    // }
-
-    // onFocused(e:any){
-    //   // do something when input is focused
-    // }
+        console.log(res.engineers);
+        this.engineers = res.engineers;
+      });
   }
+
+  // onChangeSearch(val: string) {
+  //   console.log(val)
+  //   // fetch remote data from here
+  //   // And reassign the 'data' which is binded to 'data' property.
+  // }
+
+  // onFocused(e:any){
+  //   // do something when input is focused
+  // }
+}
