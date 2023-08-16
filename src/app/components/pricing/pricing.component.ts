45231345abd6dc9811db1f5fec8aad35e72d4a82
aramille1 <aramille@googlemail.com>
2023-08-16 09:43:37 +0200

    fixed clickable blur, accordion, getstarted btns

--- src/app/components/pricing/pricing.component.ts
+++ src/app/components/pricing/pricing.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-pricing',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./pricing.component.scss']
 })
 export class PricingComponent {
-
+  @ViewChild('modal', { static: true }) modal: ElementRef;
+  contactForm = this.fb.group({
+    name: [''],
+    email: [''],
+    message: ['']
+  })
+  constructor(private auth: AuthService, private router: Router, private fb: FormBuilder) { }
+  getStarted() {
+    this.auth.isLoggedIn$.subscribe({
+      next: res => {
+        if (res) {
+          this.modal.nativeElement.click();
+        } else {
+          console.log('user is not loggedin');
+          this.router.navigate(['signin'])
+        }
+      },
+      error: err => console.error(err)
+    })
+  }
 }
