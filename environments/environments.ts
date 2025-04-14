b8f596f51d63987687904f8fba5c5ceaadd78b73
Ramil Assanov <aramille@googlemail.com>
2025-04-14 16:14:33 +0200

    Update environment configuration

--- environments/environments.ts
+++ environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:3000',
+  apiUrl: 'http://localhost:3000',
   // apiUrl: 'https://reverse-job-board-backend.onrender.com',
-    apiUrl: '/api'
+    // apiUrl: '/api'
 };
