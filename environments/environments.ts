184ca2988be8c97433a30a18b7e40168a32b9929
Ramil Assanov <aramille@googlemail.com>
2025-04-19 16:47:31 +0200

    fix env

--- environments/environments.ts
+++ environments/environments.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: true,
   // apiUrl: 'http://localhost:3000',
   // apiUrl: 'https://reverse-job-board-backend.onrender.com',
-  // apiUrl: 'https://angular-talents-backend.onrender.com',
-  apiUrl: '/api',
-
+  // apiUrl: 'https://cors-anywhere.herokuapp.com/https://angular-talents-backend.onrender.com',
+  // apiUrl: '/api',
+  apiUrl: 'https://angular-talents-backend.onrender.com',
   cloudinary: {
     cloud_name: '${CLOUDINARY_CLOUD_NAME}',
     api_key: '${CLOUDINARY_API_KEY}',
