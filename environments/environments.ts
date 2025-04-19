b3de31ef1999e649d7a9da65b9ea311c9cf858c0
Ramil Assanov <aramille@googlemail.com>
2025-04-19 15:48:22 +0200

    fixed cors error3

--- environments/environments.ts
+++ environments/environments.ts
@@ -2,14 +2,9 @@ export const environment = {
   production: true,
   // apiUrl: 'http://localhost:3000',
   // apiUrl: 'https://reverse-job-board-backend.onrender.com',
-  // apiUrl: 'https://www.angulartalents.com',
-  apiUrl: '/api',
-  // recaptcha: {
-  //   siteKey: '6LdJ-BkrAAAAAE1f-Y-fC8vuM4RhsOz2q78DJ2se', // Replace with your actual site key
-  // },
-  // hcaptcha: {
-  //   siteKey: '18921824-1850-471c-a00c-cc787de25b9e', // This is hCaptcha's test key
-  // },
+  apiUrl: 'https://angular-talents-backend.onrender.com',
+  // apiUrl: '/api',
+
   cloudinary: {
     cloud_name: '${CLOUDINARY_CLOUD_NAME}',
     api_key: '${CLOUDINARY_API_KEY}',
