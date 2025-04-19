964aec9464514c607635c410dc70a35cfcba5e84
Ramil Assanov <aramille@googlemail.com>
2025-04-19 15:30:20 +0200

    fixed cors error

--- environments/environments.ts
+++ environments/environments.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   // apiUrl: 'http://localhost:3000',
   // apiUrl: 'https://reverse-job-board-backend.onrender.com',
-  apiUrl: 'https://angulartalents.com',
+  apiUrl: 'https://www.angulartalents.com',
   // apiUrl: '/api',
   // recaptcha: {
   //   siteKey: '6LdJ-BkrAAAAAE1f-Y-fC8vuM4RhsOz2q78DJ2se', // Replace with your actual site key
