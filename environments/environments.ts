0a89d0694494b9bfd672b3584134ede90180c3a8
Ramil Assanov <aramille@googlemail.com>
2025-04-21 16:16:54 +0200

    accedently commited local env

--- environments/environments.ts
+++ environments/environments.ts
@@ -1,10 +1,10 @@
 export const environment = {
-  production: false,
+  production: true,
   // Direct backend URL - use this if CORS is properly configured on the backend
-  // apiUrl: 'https://angular-talents-backend.onrender.com',
+  apiUrl: 'https://angular-talents-backend.onrender.com',
 
   // If using proxy is necessary, uncomment this instead:
-  apiUrl: '/api',
+  // apiUrl: '/api',
 
   cloudinary: {
     cloud_name: '${CLOUDINARY_CLOUD_NAME}',
